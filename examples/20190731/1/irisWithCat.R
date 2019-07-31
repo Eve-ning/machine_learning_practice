b0f9565fa89eb0c5506131c34ae0be79c5fb023a
Evening <johnchangqi@hotmail.com>
2019-07-31 17:04:50 +0800

    Update with confusion matrix

--- examples/20190731/1/irisWithCat.R
+++ examples/20190731/1/irisWithCat.R
@@ -13,3 +13,28 @@ iris.nn <- nnet(setosa + versicolor + virginica ~ Sepal.Length + Sepal.Width + P
 print(iris.nn)
 plotnet(iris.nn)
 garson(iris.nn)
+
+iris.nn2 <- neuralnet(setosa + versicolor + virginica ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width,
+                      iris.new, hidden = c(5,5),threshold = 0.01, lifesign = 'full')
+
+plot(iris.nn2)
+iris.nn2$covariate # Importance of input
+iris.nn2$weights # Weights, Bias first then the following neurons
+iris.nn2$net.result # Classification
+
+# Create a confusion matrix to tabulate results
+
+require(caret)
+require(reshape2)
+
+net.result <-
+  as.data.frame(iris.nn2$net.result) %>% 
+  set_colnames(c("setosa", "versicolor", "virginica")) %>% 
+  tibble::rowid_to_column() %>% 
+  melt(id.vars = 1, variable.name = "species", value.name = "probability") %>% 
+  group_by(rowid) %>% 
+  mutate(selected = max(probability) == probability) %>% 
+  filter(selected == TRUE)
+
+# 100% Accuracy
+confusionMatrix(net.result$species, iris$Species)
